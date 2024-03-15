8635d092ab7f63ab69710bc08ae1565df4969728
rafaellealdantas <119167570+rafaellealdantas@users.noreply.github.com>
2024-03-15 10:15:11 -0300

    Add files via upload

---
+++ ListaExercicios/Ex06A/Program.cs
@@ -0,0 +1,26 @@
+﻿//criar um vetor
+Console.WriteLine();
+int[] vetor = new int[100];
+Random aleatorio = new Random();
+
+
+for(int i = 0; i < vetor.Length; i++)
+{
+    vetor[i] = aleatorio.Next(vetor.Length);      
+}
+
+for(int i = 0; i < vetor.Length - 1; i++)
+{
+    if (vetor[i] > vetor[i + 1])
+    {
+        int aux = vetor [i];
+        vetor[i] = vetor[i + 1];
+        vetor [i + 1] = aux;
+    }
+}
+
+
+for(int i = 0; i < vetor.Length; i++)
+{
+    Console.Write(vetor[i] + " ");
+}

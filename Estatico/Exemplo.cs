a869ec843406e046ec30f96ffe9e3b76afd91621
rafaellealdantas <119167570+rafaellealdantas@users.noreply.github.com>
2024-05-22 09:55:35 -0300

    Add files via upload

---
+++ Estatico/Exemplo.cs
@@ -0,0 +1,9 @@
+using System;
+class Exemplo
+{
+    public static void soma(int n1 , int n2)
+    {
+        Console.WriteLine(n1 + n2);
+    }
+    public static string nome = " rafael";
+}
---
+++ Estatico/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Estatico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Exemplo.soma(3 , 7);
+        Console.WriteLine("olá" + Exemplo.nome);
+        }
+        
+    }
+}

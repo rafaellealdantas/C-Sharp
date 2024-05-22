17e02ad6d24195d96713871f15a138a9d7b267ac
rafaellealdantas <119167570+rafaellealdantas@users.noreply.github.com>
2024-05-22 15:29:15 -0300

    Delete Polimorfismo directory

--- Polimorfismo/Atendente.cs
+++
@@ -1,8 +0,0 @@
-using System;
-class Atendente : Imposto
-{
-        public override void valeAlimentacao(double salario)
-    {
-        Console.WriteLine("Desconto do atendente no vale alimentação: " + (salario * 0.12));
-    }
-}
--- Polimorfismo/Gerente.cs
+++
@@ -1,8 +0,0 @@
-using System;
-class Gerente : Imposto
-{
-    public override void valeAlimentacao(double salario)
-    {
-        Console.WriteLine("Desconto do gerente no vale alimentação: " + (salario * 0.15));
-    }
-}
